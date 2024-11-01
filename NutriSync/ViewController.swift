6fa8e78d5348271e9a21baea5968edc4cf7de397
David Schaeffer <code@dschaeffer.com>
2024-11-01 11:42:37 -0400

    Initial Commit

---
+++ NutriSync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NutriSync
+//
+//  Created by David Schaeffer on 11/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NutriSyncTests/NutriSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  NutriSyncTests.swift
+//  NutriSyncTests
+//
+//  Created by David Schaeffer on 11/1/24.
+//
+
+import Testing
+@testable import NutriSync
+
+struct NutriSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
