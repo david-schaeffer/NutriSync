abd3e808b0cfc1d255f41a75e502e3845239faea
dabdo04 <abdo.d@northeastern.edu>
2024-11-07 14:16:36 -0500

    example file

--- NutriSync/Feed Screen/FeedView.swift
+++ NutriSync/Feed Screen/FeedView.swift
@@ -8,13 +8,20 @@
 import UIKit
 
 class FeedView: UIView {
+    
+   
+    var WelcomeLabel: UILabel
+    var NameTextField: UITextField
+    
+    
+    
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
 }
